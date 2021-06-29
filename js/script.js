2f9efa98aaba42e825dc6b02baa212620f766661
Floopy_frog <bilibob91@gmail.com>
2021-06-29 22:49:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+"use strict";
+
+// alert('Hello');
+
+// const result= confirm("Are you here");
+
+// console.log(result);
+
+// const answer=prompt("Are you 18",'18');
+// console.log(typeof(answer));
+
+// const answer=prompt("Are you 18",'18');
+// console.log(answer+5); строка + число
+
+// const answer=+prompt("Are you 18",'18');
+// console.log(answer+5); число + число
+
+// const answer = [];
+
+// // answer[0]=prompt("Ваше имя",'');
+// // answer[1]=prompt('Ваш возраст','');
+// // answer[2]=prompt('Ваш пол мужской?','no');
+
+// prompt, confirm, alert- работают только в брауз.
+// document.write(typeof(answer)); Массив это
+//                 это частный случай object
+
+// console.log(typeof(null)); выдаст obj что признано
+                    // официальной ошибкой
+
+
+                    // Интерполяция
+// const category='toys';
+// console.log('http://someURL.com/'+category+'/'+'4');
+
+// const category='toys';
+// console.log(`http://someURL.com/${category}/5`);
+// Для компоновки строки. Обратить внимание на ковычки!!! 
+
+// const user="Ivan";
+// alert(`Привет${user}!`);
+// Второй пример Интерполяции.
+
+// конкотинация
+// Если сложить строку и число получится строка
+// console.log('arr'+'-object');  arr-object
+// console.log(4 + '-object'); 4-object
+// console.log(4 + +'-object'); NaN ошибка Note a number
+// console.log(4 + -'-5'); 9
+
+// инкримент и дикримент
+// let incr=10,
+    // decr=10;
+
+// incr++; Постфиксная форма инкримента
+// decr--; Постфиксная форма дискримента
+
+// ++incr; Префексная форма инкримента
+// --decr; Префексная форма дискримента
+
+// console.log(++incr);
+// console.log(decr--);
+
+// %-остаток при делении
+// = -присваивание
+// == -равенство (true false) сравнивает по значению
+                            // без типов данных
+// === -строгое равенство с типом данных(true false)
+// && -Оператор"И" возвр. правду если оба верны 
+// || -или 
+// ! -оператор отрицания
+// const isCheked=true,
+//     isClose= true;
+// console.log(isCheked&&isClose);
+// console.log(isClose||isCheked);
+
+// const isCheked=false,
+//       isClose= false;
+// console.log(isClose||!isCheked);
+
+
